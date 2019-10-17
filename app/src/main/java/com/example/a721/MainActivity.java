fd3c930e5603b5ae5373e915ad3ef3508a89b084
Anastasia Shtrezer <shtrezer957@gmail.com>
2019-10-17 14:53:23 +0300

    initial commit

--- app/src/main/java/com/example/a721/MainActivity.java
+++ app/src/main/java/com/example/a721/MainActivity.java
@@ -6,28 +6,46 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText EditText;
+    Button mBtnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         EditText = findViewById(R.id.EditText);
+        mBtnSearch = findViewById(R.id.search);
+
+
+        mBtnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchClick();
+            }
+        });
+
     }
 
-    public void onClick(View view) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        if (Character.isLetter(EditText.getText().charAt(0))) {
-            Uri uri = Uri.parse("geo:?q=" + EditText.getText().toString());
-            intent.setData(uri);
-        } else {
-            Uri uri = Uri.parse("geo:" + EditText.getText().toString());
-           intent.setData(uri);
+    private void searchClick() {
+        String searchString = EditText.getText().toString();
+        String param = "geo:?q=";
+        if (searchString.indexOf(", ") != -1 & searchString.matches("[\\d,. ]+")){
+            param = "geo:";
         }
-        startActivity(intent);
+        Uri objectName = Uri.parse(param + searchString);
+        Intent findObject = new Intent(Intent.ACTION_VIEW, objectName);
+        startActivity(findObject);
+
     }
+
+
 }
+
+
